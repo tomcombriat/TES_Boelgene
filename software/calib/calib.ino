16eac7eaf2ceeace20e658e1d8304568c93293b5
tomcombriat <tomcombriat@live.fr>
2025-02-06 23:03:07 +0100

    Added calibration script

---
+++ software/calib/calib.ino
@@ -0,0 +1,30 @@
+/*
+Calibration script for Boelgene v1.
+*/
+
+
+#include <SPI.h>
+#include <MCP3XXX.h>
+
+
+MCP3XXX_<10, 8, 300000> adc;
+
+void setup() {
+  Serial.begin(115200);
+  analogReadResolution(12);
+  SPI.setRX(16);
+  SPI.setTX(19);
+  SPI.setSCK(18);
+  adc.begin(17);
+}
+
+void loop() {
+  Serial.print(analogRead(26));
+  Serial.print(" ");
+  Serial.print(adc.analogRead(0));
+  Serial.print(" ");
+  Serial.print(adc.analogRead(1));
+  Serial.print(" ");
+  Serial.println(adc.analogRead(2));
+  delay(100);
+}
